67fa809c5b83a4fccec7d63f4a6cdd10ddfdbc33
LuckyLazySheep <460109381@qq.com>
2026-08-15 00:38:42 +0800

    # feat     : 新增请求类型系统基础层（HttpMethod、参数标签、ValueTypeOf 映射）

--- include/request/HttpRequest.h
+++ include/request/HttpRequest.h
@@ -3,7 +3,6 @@
 #include <string_view>
 
 #include "HttpRequest.h"
-#include "HttpRequest.h"
 
 namespace NetCore {
 
@@ -50,5 +49,99 @@ namespace NetCore {
       _STR_8(s, 32), _STR_8(s, 40), _STR_8(s, 48), _STR_8(s, 56)
 
 #define STR(s) typename NetCore::BuildStr<NetCore::StrLiteral<>, _STR_64(s)>::type
+
+
+	// ---------- HTTP 方法 ----------
+	enum class HttpMethod {
+		GET,
+		POST,
+		PUT,
+		DEL,
+		PATCH
+	};
+
+	// ----------参数位置标签（Tag Dispatch）----------
+	struct PathTag {};
+	struct QueryTag {};
+	struct FormTag {};
+	struct HeaderTag {};
+	struct BodyTag {};
+
+	// ---------- 值类型标签 ----------
+	struct TypeString {};
+	struct TypeQString {};
+	struct TypeInt {};
+	struct TypeFloat {};
+	struct TypeBool {};
+	struct TypeJson {};
+	struct TypeFile {};
+	struct TypeBinary {};
+
+	template <typename T>
+	struct TypeModel {};
+
+	// ---------- FileValue：文件上传值 ----------
+	struct FileValue {
+		QString filePath;
+		QByteArray data;
+		QString fileName;
+		QString contentType;
+
+		static FileValue fromPath(const QString& path, const QString& contentType = {})
+		{
+			FileValue value;
+			value.filePath = path;
+			value.contentType = contentType;
+			return value;
+		}
+
+		static FileValue fromBytes(const QByteArray& data,
+			const QString& fileName,
+			const QString& contentType = {})
+		{
+			FileValue value;
+			value.data = data;
+			value.fileName = fileName;
+			value.contentType = contentType;
+			return value;
+		}
+	};
+
+	// ---------- ValueTypeOf：值类型标签 → C++ 类型映射 ----------
+	template <typename T>
+	struct ValueTypeOf;
+
+	template <>
+	struct ValueTypeOf<TypeString> { using type = std::string_view; };
+
+	template <>
+	struct ValueTypeOf<TypeQString> { using type = QString; };
+
+	template <>
+	struct ValueTypeOf<TypeInt> { using type = int64_t; };
+
+	template <>
+	struct ValueTypeOf<TypeFloat> { using type = double; };
+
+	template <>
+	struct ValueTypeOf<TypeBool> { using type = bool; };
+
+	template <>
+	struct ValueTypeOf<TypeJson> { using type = QJsonObject; };
+
+	template <>
+	struct ValueTypeOf<TypeFile> { using type = FileValue; };
+
+	template <>
+	struct ValueTypeOf<TypeBinary> { using type = QByteArray; };
+
+	template <typename T>
+	struct ValueTypeOf<TypeModel<T>> { using type = T; };
+
+	// ---------- NoKeyStr：Body 等无键名参数的占位 KeyStr ----------
+	struct NoKeyStr {
+		static constexpr std::string_view view() { return ""; }
+		static constexpr const char* c_str() { return ""; }
+	};
 
 }
--- tests/tst_httprequest.cpp
+++ tests/tst_httprequest.cpp
@@ -1,31 +1,73 @@
 #include <QtTest>
+#include <QByteArray>
+#include <QString>
 #include <type_traits>
 #include <request/HttpRequest.h>
 
 static_assert(std::is_same<STR("hello"), STR("hello")>::value,
-    "same string must map to same type");
+	"same string must map to same type");
 static_assert(!std::is_same<STR("hello"), STR("world")>::value,
-    "different strings must map to different types");
+	"different strings must map to different types");
+
+struct MyStruct {
+};
+
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeInt>::type, int64_t>,
+	"TypeInt must map to int64_t");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeString>::type, std::string_view>,
+	"TypeString must map to std::string_view");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeFloat>::type, double>,
+	"TypeFloat must map to double");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeBool>::type, bool>,
+	"TypeBool must map to bool");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeQString>::type, QString>,
+	"TypeQString must map to QString");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeJson>::type, QJsonObject>,
+	"TypeJson must map to QJsonObject");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeFile>::type, NetCore::FileValue>,
+	"TypeFile must map to FileValue");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeBinary>::type, QByteArray>,
+	"TypeBinary must map to QByteArray");
+static_assert(std::is_same_v<NetCore::ValueTypeOf<NetCore::TypeModel<MyStruct>>::type, MyStruct>,
+	"TypeModel<T> must map to T");
+
+
+static_assert(NetCore::NoKeyStr::view().empty(), "NoKeyStr view must be empty");
+static_assert(NetCore::NoKeyStr::c_str()[0] == '\0', "NoKeyStr c_str must be empty");
 
 class TestHttpRequest : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 
 private slots:
-    void strView();
-    void strSize();
+	void strView();
+	void strSize();
+	void fileValueFactories();
 };
 
 void TestHttpRequest::strView()
 {
-    const auto view = STR("hello")::view();
-    QCOMPARE(QString::fromLatin1(view.data(), static_cast<int>(view.size())),
-        QStringLiteral("hello"));
+	const auto view = STR("hello")::view();
+	QCOMPARE(QString::fromLatin1(view.data(), static_cast<int>(view.size())),
+		QStringLiteral("hello"));
 }
 
 void TestHttpRequest::strSize()
 {
-    QCOMPARE(STR("hello")::view().size(), size_t(5));
+	QCOMPARE(STR("hello")::view().size(), size_t(5));
+}
+
+void TestHttpRequest::fileValueFactories()
+{
+	const NetCore::FileValue fromDisk = NetCore::FileValue::fromPath(QStringLiteral("a.txt"));
+	QCOMPARE(fromDisk.filePath, QStringLiteral("a.txt"));
+	QVERIFY(fromDisk.data.isEmpty());
+
+	const NetCore::FileValue fromMemory =
+		NetCore::FileValue::fromBytes(QByteArray("hello"), QStringLiteral("b.txt"));
+	QCOMPARE(fromMemory.data, QByteArray("hello"));
+	QCOMPARE(fromMemory.fileName, QStringLiteral("b.txt"));
+	QVERIFY(fromMemory.filePath.isEmpty());
 }
 
 QTEST_GUILESS_MAIN(TestHttpRequest)
